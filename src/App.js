f8b9eb9d588877123ef146fa0ce397036074779c
emmanuelc1 <emmanuelcastillo1@gmail.com>
2023-08-10 20:29:38 -0700

    adds useReducer state and fetches local API data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import DateCounter from './DateCounter.js';
+import { useEffect, useReducer } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+
+const intialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, intialState);
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(err => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
